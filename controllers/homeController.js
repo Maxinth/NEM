0419ab0f5660337e3d0f81f7ba3759e457db9fa0
maxinth <tunde.j.adeleye@gmail.com>
2021-06-08 15:31:07 +0100

    removed controllers directory

--- controllers/homeController.js
+++
@@ -1,9 +0,0 @@
-exports.sendReqParams = (req, res) => {
-  let veg = req.params.vegetable;
-  res.send(`This is the page for ${veg}`);
-};
-
-exports.respondWithName = (req, res) => {
-  let paramsName = req.params.myName;
-  res.render("index", { name: paramsName }); // index here being the index.ejs file inside views
-};
