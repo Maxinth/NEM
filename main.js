01d7f103af647b43e9c450c97b156e16bffc9266
maxinth <tunde.j.adeleye@gmail.com>
2021-09-22 13:59:14 +0100

    saving files at the repl

--- main.js
+++ main.js
@@ -6,15 +6,26 @@ let messages = [
   "Just run with the code",
 ];
 
+/*
+
+TO SAVE FILE-SESSIONS using .save at the repl
+
+load a file in , say main.js
+
+- SUPPLY node at terminal
+- .load main.js
+
+- *******
+repl now has access to messages as it has just been loaded in.
 messages.forEach((msg) => console.log(` * ${msg}`));
 
-/*
-To load in this file 
+after getting the output
 
-supply node at terminal 
+all that has been done can be saved to a file, say a file inside the moreTests folder called displayMsg.js
+this is done by supplying .save moreTests/displayMsg.js at the repl
 
-THEN
 
-.load main.js
+
+
 
 */
