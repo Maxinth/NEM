330a302442c2d7183a8eaab178e8f74b66a3f63d
maxinth <tunde.j.adeleye@gmail.com>
2021-09-22 18:13:29 +0100

    returning json data as a response

--- main.js
+++ main.js
@@ -1,13 +1,24 @@
-import { messages } from "./moreTests/messages.js";
-import { names } from "./names.js";
-/* 
+import httpStatus from "http-status-codes";
+import http from "node:http";
+import {data} from './db.js'
 
-        when using es6 imports, file extension names '.js' have to be included.
-        Leaving it out will lead to an error.
+const port = 5000;
+const { createServer } = http;
 
-*/
+// create the server
+const app = createServer((req, res) => {
+  console.log("Received an incoming request!");
+  res.writeHead(httpStatus.OK, {
+    "Content-Type": "text/html",
+  });
 
-console.log(messages);
+//   let responseMsg = "<h1>Hello, Node!</h1>";
+//   res.write(responseMsg);
+  res.write(JSON.stringify(data))
+  //   res.write("sending a string instead of html");
+  res.end(); // close server connection right after the responses have been sent
+  // console.log(`Sent a response: ${responseMsg}`);
+});
 
-console.log("msg = ", messages);
-console.log("names =", names);
+app.listen(port);
+console.log(`The server has started and is listening on port number: ${port}`);
