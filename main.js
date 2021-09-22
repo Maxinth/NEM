1e3771d2293d4c83f6dcd69f76ffc0e6fe7e2b43
maxinth <tunde.j.adeleye@gmail.com>
2021-09-22 15:08:11 +0100

    using es6 imports in node - first go

--- main.js
+++ main.js
@@ -1,31 +1,4 @@
-"use strict";
-
-let messages = [
-  "A change of environment can be a good thing!",
-  "You will make it",
-  "Just run with the code",
-];
-
-/*
-
-TO SAVE FILE-SESSIONS using .save at the repl
-
-load a file in , say main.js
-
-- SUPPLY node at terminal
-- .load main.js
-
-- *******
-repl now has access to messages as it has just been loaded in.
-messages.forEach((msg) => console.log(` * ${msg}`));
-
-after getting the output
-
-all that has been done can be saved to a file, say a file inside the moreTests folder called displayMsg.js
-this is done by supplying .save moreTests/displayMsg.js at the repl
-
-
-
-
-
-*/
+const messages = require("./moreTests/messages");
+import { messages } from "./moreTests/messages.js";
+import { names } from "./names.js";
+console.log(messages);
---
+++ moreTests/messages.js
@@ -0,0 +1,12 @@
+"use strict";
+
+let messages = [
+  "A change of environment can be a good thing!",
+  "You will make it",
+  "Just run with the code",
+];
+
+messages.forEach((msg) => console.log(` * ${msg}`));
+
+module.exports = messages;
+// export { messages};
